8e082bfdfbf9d78644a4f8e2089b21a00ba962bd
unknown <hamza23@seznam.cz>
2024-06-03 03:48:42 +0200

    not styled form

--- Web App/www/html/add-recipe-form.php
+++ Web App/www/html/add-recipe-form.php
@@ -6,6 +6,25 @@
 <html>
     <body>
         <form action="add-recipe-form.php" method="post">
+            <label>Name</label>
+            <input type="text" name="name"/>
+            
+            <label>Mealtype</label>
+            <input type="text" name="mealtype"/>
+            <label>Amount</label>
+            <input type="text" name="amount"/>
+            
+            <label>Ingredients</label>
+            <input type="text" name="ingredients"/>
+            
+            <label>Duration</label>
+            <input type="text" name="duration"/>
+            
+            <label>Instructions</label>
+            <input type="text" name="instructions"/>
+            <input type="submit"/>
         </form>
     </body>
 </html>
+
+
